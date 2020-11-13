282050ec5c9c80ed5b00f8ceed31aaf9cc19a521
Cyan Villarin <cyanvillarin@gmail.com>
2020-11-13 11:42:08 +0900

    Tested API call

--- Movie db/ViewController.swift
+++ Movie db/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
+      fetchMovie()
+   }
+   
+   func fetchMovie() {
+
+      let endpoint = "http://www.omdbapi.com/?apikey=9b67ae8d"
+      
+      Alamofire.request(endpoint, method: .get).responseJSON { (response) -> Void in
+        
+        if let error = response.error {
+            print("Error = \(error.localizedDescription)")
+            print("=========\n")
+        }
+        
+        if let result = response.result.value {
+            let result = JSON(result)
+            print(result)
+            print("=========\n")
+        }
+      }
    }
 
 
